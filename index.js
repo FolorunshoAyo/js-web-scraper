56063253cef4e7289ad0342b00f6bb566d7faff6
FolorunshoAyo <folushoayomide11@gmail.com>
2023-03-29 09:27:15 +0100

    New Implementation
    Reconstructed my scraper to scrape Mitutoyo sites

--- index.js
+++ index.js
@@ -8,26 +8,29 @@ let browserInstance = startBrowser();
 scraperController(browserInstance, 
     [
         {
-            csvFile:"marine-engine-parts.csv", 
+            csvFile:"mitutoyo-shop-links-test.csv", 
             ids: 
                 {
-                    product_title: {
-                        selector: "span[itemprop='name']",
+                    product_name: {
+                        selector: ".product-content .name h2",
                         type: "text"
                     },
-                    product_image: {
-                        selector:  "img.fotorama__img",
-                        type: "image"
-                    }, 
                     product_sku:{
-                        selector: "div[itemprop='sku']",
+                        selector: ".product-content .anteros span.value",
                         type: "text"
                     },
-                    product_details: {
-                        selector: ".table-wrapper",
+                    product_price: {
+                        selector: ".listPrice span.value:nth-of-type(1)",
+                        type: "text"
+                    },
+                    product_description: {
+                        selector: "div.description-big",
                         type: "HTML"
                     },
-
+                    product_features: {
+                        selector: "#product_parameters",
+                        type: "HTML"
+                    },
                 }
         }
     ])
--- pageScraper.js
+++ pageScraper.js
@@ -1,19 +1,31 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
+const ObjectsToCsv = require('objects-to-csv'); 
 
 let links = [];
 let finalResult = [];
 let index = 0;
 
 function handleData (data){
-    links[index] = [...links[index], ...data];
+    links.push(data[0]);
+}
+
+function addToResults(data){
+    finalResult.push(data);
+
+    if(links.length === finalResult.length){
+        console.log("Final ", finalResult);
+
+        (async () => {
+            const csv = new ObjectsToCsv(finalResult);
+        
+            // Save to file:
+            await csv.toDisk('./test.csv');
+        })();
+    }
 }
 
 function scraper(browser, scraperInfo) {
-    // CREATE NEW ARRAY LIST FOR LINKS AND RESULTS
-    links[index] = [];
-    finalResult[index] = [];
-
     for(const pageDetails of scraperInfo){
         // Converting each csv file to JSON
         fs.createReadStream(pageDetails.csvFile)
@@ -35,29 +47,84 @@ function scraper(browser, scraperInfo) {
 
                         await newPage.goto(link);
 
-                        for(const key1 in pageDetails.ids){
-                            for(const key2 in pageDetails[key1]){
-                                if(pageDetails.ids[key1][key2].type.toLowerCase() === "image") {
-                                    dataObj[key1] = await newPage.$eval(pageDetails.selectors[key], el => el.src)
-                                }else if(pageDetails.ids[key1][key2].type.toLowerCase() === "html"){
-                                    dataObj[key1] = await newPage.$eval(pageDetails.selectors[key], el => el.innerHTML);
-                                }else{
-                                    dataObj[key1] = await newPage.$eval(pageDetails.selectors[key], el => el.textContent);
-                                }
+                        // for(const key1 in pageDetails.ids){
+                            // if(pageDetails.ids[key1].type.toLowerCase() === "image") {
+                            //     dataObj[key1] = await newPage.$eval(pageDetails.ids[key1].selector, el => el.src)
+                            // }else if(pageDetails.ids[key1].type.toLowerCase() === "html"){
+                            //     dataObj[key1] = await newPage.evaluate(() => {
+                            //         const element = document.querySelector(pageDetails.ids[key1].selector);
+
+                            //         if(element){
+                            //             return element.innerHTML;
+                            //         }else{
+                            //             return "";
+                            //         }
+                            //     });
+                            // }else{
+                            //     dataObj[key1] = await newPage.evaluate(function () {
+                                    
+                            //         const element = document.querySelector(pageDetails.ids[key1].selector);
+
+                            //         if(element){
+                            //             return element.textContent;
+                            //         }else{
+                            //             return "";
+                            //         }
+                            //     });
+                            // }
+                        // }
+
+                        dataObj = await newPage.evaluate(function () {
+                            let scrapedData = {};
+
+                            const product_name = document.querySelector(".product-content .name h2");
+                            const product_sku = document.querySelector(".product-content .anteros span.value");
+                            const product_price = document.querySelector(".listPrice span.value:nth-of-type(1)");
+                            const product_description = document.querySelector("div.description-big");
+                            const product_features = document.querySelector("#product_parameters");
+
+                            scrapedData = {
+                                product_name: product_name.textContent,
+                                product_sku: product_sku? product_sku.textContent : "",
+                                product_price: product_price? product_price.textContent : "",
+                                product_description: product_description.innerHTML,
+                                product_features: product_features.innerHTML
                             }
+
+                            return scrapedData;
+                        });
+
+                        // MITUTOYO ADDITION FOR SCRAPING IMAGES
+                        await newPage.click("img.clickable", {delay: 500});
+
+                        if(await newPage.waitForSelector("img.cboxPhoto")){
+                            dataObj.product_image = await newPage.evaluate(() => {
+                                const element = document.querySelector("img.cboxPhoto");
+
+                                if(element){
+                                    return element.src;
+                                }else{
+                                   return "";
+                                }
+
+                            });
                         }
-                            
+
+                        console.log(dataObj);
+
                         resolve(dataObj);
                         await newPage.close();
                     });
 
                     // Visiting all the links
-                    for(const link of links[index]){
-                        let scrappedData = await pagePromise(link);
-                        finalResult[index].push(scrappedData);
+                    for(const link of links){
+                        let scrapedData = await pagePromise(link);
+                        addToResults(scrapedData);
                     }
 
-                } catch(e) { }
+                } catch(e) {
+                    console.log(e);
+                }
             
                 await browser.close();
             }
